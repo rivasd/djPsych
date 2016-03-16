6642d504f02c0522ecf38752339d2681a1d34dad
rivasd <rivasdaniel1992@gmail.com>
2016-03-16 16:22:50 -0400

    studied solutions for generic m2m relations...

--- djstim/models/BasicStimuli.py
+++ djstim/models/BasicStimuli.py
@@ -6,7 +6,7 @@ Created on Feb 24, 2016
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as l_
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 import json
 
@@ -15,7 +15,8 @@ class LinkedStimulus(models.Model):
     """
     Model used to implemented Generic many to many relations between a setting object and a stimuli object
     
-    
+    Since djPsych is heavily centered around the django admin, we need to be able to add stimuli objects to stuff as Inlines
+    Both librairies adressing generic m2m relationships that I found are poor and, critically, offer no admin integration.
     """
     # First the link to the setting object
     setting_type = models.ForeignKey(ContentType)
@@ -25,7 +26,7 @@ class LinkedStimulus(models.Model):
     # now the link to the simuli object
     stim_type = models.ForeignKey(ContentType)
     stim_id = models.PositiveIntegerField()
-    stimulus = GenericForeignKey()
+    stimulus = GenericForeignKey('stim_type', 'stim_id')
     
     
     
@@ -36,9 +37,7 @@ class BaseStimuli(models.Model):
     class Meta: 
         abstract=True
         
-    block_type = models.ForeignKey(ContentType)
-    block_id = models.PositiveIntegerField()
-    block_setting = GenericForeignKey('block_type', 'block_id')
+    
     name = models.CharField(max_length=26, help_text=l_("A simple name for this particular stimuli pair"))
     index = models.PositiveSmallIntegerField(blank=True, null=True, help_text=l_("Youn can give a number to indicate the order among the pairs that point to the same block. They will be given in asceding order."))
     stimulus = models.CharField(max_length=256, help_text=l_("The path to your stimuli file inside the static files folder we provided. Or it can be a short HTML string"))
