4cb5eac0888a2c01d292c8aacded9e98d7752802
rivasd <rivasdaniel1992@gmail.com>
2016-07-26 10:34:56 -0400

    prettifying the payment admin

--- djpay/admin.py
+++ djpay/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Payment
 # Register your models here.
-admin.site.register(Payment)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    
+    list_display = ('amount', 'currency', 'receiver', 'time_created', 'status', 'sent')
+    list_editable = ('sent')
