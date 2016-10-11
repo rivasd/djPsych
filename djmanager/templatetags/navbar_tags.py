043e0e29fcd8d8766402c3161d4e5e17ab8dd097
catou239 <catou239@hotmail.com>
2016-10-11 15:05:22 -0400

    tested and fixed the plateform navbar (custom tag), bravo Catherine! :)

--- djmanager/templatetags/navbar_tags.py
+++ djmanager/templatetags/navbar_tags.py
@@ -3,9 +3,9 @@ from djexperiments.models import Experiment
 
 register = template.Library()
 
-@register.inclusion_tag('template/researcher_navbar.html', takes_context=True)
+@register.inclusion_tag('includes/researcher_navbar.html', takes_context=True)
 def show_experiments(context):
     
-    experiments = Experiment.objects.filter(research_group__in = request.user.groups.all())
+    experiments = Experiment.objects.filter(research_group__in = context['request'].user.groups.all())
     return {'experiments': experiments}
 
