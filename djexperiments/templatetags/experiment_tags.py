dd9d4d7e21bbe7c39f9394ee1be9ab0d8d01d141
rivasd <rivasdaniel1992@gmail.com>
2016-10-19 16:28:52 -0400

    IHATECSSWITHTHEPASSIONOFATHOUSANDSUNS

---
+++ djexperiments/templatetags/experiment_tags.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 19, 2016
+
+@author: rivas
+'''
+
+from django import template
+from djexperiments.models import Experiment
+
+register = template.Library()
+
+@register.inclusion_tag("custom_tag_templates/cardify.html")
+def cardify(experiment):
+    return {}
