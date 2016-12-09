f21e7e80e2f3e0515ff1723ac6bd23647323e5f0
catou239 <catou239@hotmail.com>
2016-12-09 13:36:12 -0500

    starting to structure the expbuilder app

---
+++ djexperiments/static/expbuilder/expbuilder.js
@@ -0,0 +1,20 @@
+/**
+ * Defines namespace and initiate the expBuilder app
+ */
+
+	var expBuilder = {
+		
+		module: function(){
+			var modules = {};
+			
+			return function(name){
+				
+				if(modules[name]){
+					
+					return modules[name];
+				}
+				
+				return modules[name] = {Views:{}};
+			}
+		}
+	}
