a07eadba7b7e3ba14181a101b5fe6332cf2b4ce2
catou239 <catou239@hotmail.com>
2016-12-09 17:30:09 -0500

    starting to develop backbone models

--- djexperiments/static/expbuilder/expbuilder.js
+++ djexperiments/static/expbuilder/expbuilder.js
@@ -1,20 +1,3 @@
 /**
  * Defines namespace and initiate the expBuilder app
  */
-
-	var expBuilder = {
-		
-		module: function(){
-			var modules = {};
-			
-			return function(name){
-				
-				if(modules[name]){
-					
-					return modules[name];
-				}
-				
-				return modules[name] = {Views:{}};
-			}
-		}
-	}
--- djexperiments/static/expbuilder/modules/bbexperiments.js
+++ djexperiments/static/expbuilder/modules/bbexperiments.js
@@ -1,12 +1,16 @@
 /**
- * Everything you need for the block models is here
+ * Everything you need for the block model is here
  */
-	
 
+(function(expBuilder, Block) {
+	
+	//Dependencies
+	//Shorthands
+	//The application container
 	
 	
 	//Define a basic block (with all the attributes all the blocks have in common)
-	var block = Backbone.Model.extends({
+	Block.Model = Backbone.Model.extends({
 		defaults: {
 			id: null,
 			content_type: null,
@@ -21,11 +25,6 @@
 		
 	});
 	
-	var audioCatBlock = Block.extends({
-		
-	});
 	
 	
-	
-	
-	
+})(expBuilder, expBuilder.module("block"))
