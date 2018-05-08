c67abc22402e06a557820d2ae6a01227f56df5c0
rivasd <rivasdaniel1992@gmail.com>
2018-05-08 18:12:16 -0400

    oops

--- djexperiments/utils.py
+++ djexperiments/utils.py
@@ -3,5 +3,5 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 def validate_exp_label(label):
-    if not re.match( r'^[a-zA-Z0-9][A-Za-z0-9_-]$', label):
+    if not re.match( r'^[a-zA-Z0-9][A-Za-z0-9_-]*$', label):
         raise ValidationError(_("Your experiment label must contain only letters, numbers, or underscores"))
